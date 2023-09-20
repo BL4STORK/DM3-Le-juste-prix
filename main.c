e862a199fb690e0ed254d175f3298a5114b42527
BL4STORK <144135621+BL4STORK@users.noreply.github.com>
2023-09-20 11:03:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-#include "stdlib.h"
+#include <stdlib.h>
+#include "time.h"
 
 int main() {
-    printf("Hello, World!\n");
+int rand ();
+    srand(time(NULL));
+    int randomNumber = (rand() % 1000) + 1;
+    int number;
+
+    while (randomNumber){
+        printf("Choissisez un nombre entre 1 et 1000: \n");
+        scanf("%d", &number);
+
+        if( number < randomNumber){
+            printf("C'est plus\n");
+        }if(number > randomNumber){
+            printf("C'est moins\n");
+        }if(number == randomNumber){
+            printf("Bravo, vous avez trouve le juste prix!\n");
+            return 0;
+        }
+    }
+
     return 0;
 }
